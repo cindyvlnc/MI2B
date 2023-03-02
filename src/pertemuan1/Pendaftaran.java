3336b14ef7a7d0239f3a1ea8941fcc9e6c9eb443
cindyvlnc <cindyvallensia2003@gmail.com>
2023-03-02 12:06:52 +0700

    [NF] Mahasiswa dan Pendaftaran

--- src/pertemuan1/Pendaftaran.java
+++ src/pertemuan1/Pendaftaran.java
@@ -11,6 +11,18 @@ package pertemuan1;
  */
 public class Pendaftaran {
     public static void main(String[] args){
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.nama = "Cindy";
+        mhs.nobp = "2101092004";
+        mhs.kelas = "MI2B";
+        mhs.prodi = "MI";
+        mhs.jurusan = "Teknologi Informasi";
         
+        System.out.println("Mahasiswa 1");
+        System.out.println("Nama    : " +mhs.nama);
+        System.out.println("No Bp   : " +mhs.nobp);
+        System.out.println("Kelas   : " +mhs.kelas);
+        System.out.println("Prodi   : " +mhs.prodi);
+        System.out.println("Jurusan : " +mhs.jurusan);
     }
 }
