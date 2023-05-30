9b9691b41d751a566013bb520ea50854b7d54727
cindyvlnc <116186314+cindyvlnc@users.noreply.github.com>
2023-05-30 17:59:53 +0700

    Add files via upload
    UTS CINDY VALLENSIA

---
+++ swing2/TransferModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swing2;
+
+/**
+ *
+ * @author USER
+ */
+public class TransferModel {
+    String transfer="200000";
+
+    public String getTransfer() {
+        return transfer;
+    }
+
+    public void setTransfer(String transfer) {
+        this.transfer = transfer;
+    }
+    
+}
